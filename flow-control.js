bb196097c3eacb78474c2d6d4e5d58a97597a207
Abhishek Jadhav <abhidj0001@gmail.com>
2019-08-16 07:19:11 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -28,7 +28,7 @@ function switchAge(age) {
 switch (age) {
   case (age>=13 && age<=19):  return "You are a teenager";
     break;
-  case age: return "You are a teenager1";
+  case age<19: return "You are a teenager";
     break;
   default:return "You have an age";
 
