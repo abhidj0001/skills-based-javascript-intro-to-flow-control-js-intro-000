08fcafdb67044cbfa81aee9b9608a5d83d15b805
Abhishek Jadhav <abhidj0001@gmail.com>
2019-08-16 07:48:16 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -21,12 +21,12 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-return (age>13 && age<19)?"You are a teenager":"You are not a teenager";
+return (age>=13 && age<=19)?"You are a teenager":"You are not a teenager";
 }
 
 function switchAge(age) {
   switch (age) {
-  case (age>=13 && age<=19):
+  case (age>13 && age<19):
   console.log("you are a teenager");
   break;
   default:
