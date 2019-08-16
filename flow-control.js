b3b69aa3cd06953a1a64324d7619a375572ce619
Abhishek Jadhav <abhidj0001@gmail.com>
2019-08-16 06:54:57 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -17,11 +17,11 @@ function ageChecker(age) {
   else if(age<=12)
     return "You are a kid"
     else (age>=20)
-    return "you are a grownup"
+    return "You are a grownup"
 }
 
 function ternaryTeenager(age) {
-
+if(age>=13 && age<=19)?"You are a teenager":"You are not a teenager"
 }
 
 function switchAge(age) {
